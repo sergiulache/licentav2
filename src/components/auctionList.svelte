4600d27651f514c1220aa14b3e38756c852ff001
phatiacy <phatiacyy@gmail.com>
2023-05-19 19:01:57 +0300

    pagination final

--- src/components/auctionList.svelte
+++ src/components/auctionList.svelte
@@ -30,36 +30,39 @@
 		currentPage * itemsPerPage
 	); // display only items for current page
 	$: pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1); // create array of page numbers
+
+	$: displayedPageNumbers = pageNumbers.reduce((acc, page) => {
+		if (page === 1 || page === totalPages || (page >= currentPage - 2 && page <= currentPage + 2)) {
+			acc.push(page);
+		} else if (page === currentPage - 3 || page === currentPage + 3) {
+			acc.push('...');
+		}
+		return acc;
+	}, []);
 </script>
 
 <div class="bg-white shadow overflow-hidden sm:rounded-md p-4 sm:ml-64">
 	<div class="columns margins">
-		<div class="autocomplete-container">
+		<div class="mt-1 relative rounded-md shadow-sm">
 			{#if isLoading}
 				<div style="display: flex; justify-content: center">
 					<div class="loader" />
 				</div>
 			{:else}
-				<div>
-					<label for="search" class="block text-sm font-medium text-gray-700">Search Auctions</label
-					>
-					<div class="mt-1 relative rounded-md shadow-sm">
-						<input
-							bind:value={searchQuery}
-							type="text"
-							name="search"
-							id="search"
-							class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
-							placeholder="Search..."
-						/>
-						<button
-							on:click={search}
-							class="absolute inset-y-0 right-0 px-3 flex items-center bg-indigo-500 text-white rounded-r-md cursor-pointer"
-						>
-							Search
-						</button>
-					</div>
+				<div class="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
+					<span class="text-gray-500 sm:text-sm"> 🔍 </span>
 				</div>
+				<input
+					bind:value={searchQuery}
+					type="text"
+					class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+					placeholder="Search Auctions"
+				/>
+				<button
+					on:click={search}
+					class="absolute inset-y-0 right-0 px-3 flex items-center bg-indigo-500 text-white rounded-r-md"
+					>Search</button
+				>
 			{/if}
 		</div>
 	</div>
@@ -70,30 +73,67 @@
 			</li>
 		{/each}
 	</ul>
-	<div class="flex justify-center my-4">
-		<button
-			on:click={() => (currentPage = currentPage > 1 ? currentPage - 1 : 1)}
-			class="mx-2 bg-indigo-500 text-white px-4 py-2 rounded-md"
-		>
-			Previous
-		</button>
-		{#each pageNumbers as page}
-			<button
-				on:click={() => (currentPage = page)}
-				class="mx-2 px-4 py-2 rounded-md text-white {currentPage === page
-					? 'bg-indigo-700'
-					: 'bg-indigo-500'}"
-			>
-				{page}
-			</button>
-		{/each}
-		<button
-			on:click={() => (currentPage = currentPage < totalPages ? currentPage + 1 : totalPages)}
-			class="mx-2 bg-indigo-500 text-white px-4 py-2 rounded-md"
-		>
-			Next
-		</button>
-	</div>
+	<nav
+		class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+		aria-label="Pagination"
+	>
+		<div class="flex-1 flex justify-between sm:hidden">
+			{#if currentPage !== 1}
+				<button
+					on:click={() => currentPage--}
+					class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 hover:text-gray-500"
+				>
+					Previous
+				</button>
+			{/if}
+			{#if currentPage !== totalPages}
+				<button
+					on:click={() => currentPage++}
+					class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+				>
+					Next
+				</button>
+			{/if}
+		</div>
+		<div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+			<div>
+				<p class="text-sm text-gray-700">
+					<span class="font-medium">{(currentPage - 1) * itemsPerPage + 1}</span>
+					<span class="font-medium">-</span>
+					<span class="font-medium">{currentPage * itemsPerPage}</span>
+					<span>of</span>
+					<span class="font-medium">{filteredAuctions.length}</span>
+					<span>results</span>
+				</p>
+			</div>
+			<div>
+				<nav
+					class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+					aria-label="Pagination"
+				>
+					{#each displayedPageNumbers as page}
+						{#if page === '...'}
+							<span
+								class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+							>
+								...
+							</span>
+						{:else}
+							<button
+								class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-blue-100 {currentPage ===
+								page
+									? 'bg-green-200'
+									: ''}"
+								on:click={() => (currentPage = page)}
+							>
+								{page}
+							</button>
+						{/if}
+					{/each}
+				</nav>
+			</div>
+		</div>
+	</nav>
 </div>
 
 <style>
